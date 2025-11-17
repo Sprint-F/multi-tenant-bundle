3c547fd7558f0a29639b44fe6f7ddbf7ebd48d36
Albert Stepantsev <albert.stepantsev@sprintf.ru>
2025-11-17 15:20:13 +0300

    + TenantContext

--- src/Context/TenantContext.php
+++ src/Context/TenantContext.php
@@ -3,11 +3,13 @@
 namespace SprintF\Bundle\MultiTenant\Context;
 
 use SprintF\Bundle\MultiTenant\Tenant\TenantInterface;
+use Symfony\Component\DependencyInjection\Attribute\AsAlias;
 
 /**
  * Стандартная реализация интерфейса TenantContextInterface:
  * текущий контекст арендатора.
  */
+#[AsAlias]
 class TenantContext implements TenantContextInterface
 {
     private ?TenantInterface $tenant = null;
--- src/SprintFMultiTenantBundle.php
+++ src/SprintFMultiTenantBundle.php
@@ -2,8 +2,16 @@
 
 namespace SprintF\Bundle\MultiTenant;
 
+use SprintF\Bundle\MultiTenant\Context\TenantContext;
+use SprintF\Bundle\MultiTenant\Context\TenantContextInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class SprintFMultiTenantBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yaml');
+    }
 }
