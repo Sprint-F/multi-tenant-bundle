90e80d6d0cf82f54c40123bc49054557fbd771d6
Albert Stepantsev <albert.stepantsev@sprintf.ru>
2025-11-17 17:27:00 +0300

    + bundle config

--- src/SprintFMultiTenantBundle.php
+++ src/SprintFMultiTenantBundle.php
@@ -2,14 +2,23 @@
 
 namespace SprintF\Bundle\MultiTenant;
 
-use SprintF\Bundle\MultiTenant\Context\TenantContext;
-use SprintF\Bundle\MultiTenant\Context\TenantContextInterface;
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class SprintFMultiTenantBundle extends AbstractBundle
 {
+    public function configure(DefinitionConfigurator $definition): void
+    {
+        $definition->rootNode('')
+            ->children()->scalarNode('tenant_entity')
+                ->defaultValue('\App\Entity\Tenant')
+                ->info('The fully qualified class name of tenant entity')
+            ->end() // tenant_entity
+        ;
+    }
+
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         $container->import('../config/services.yaml');
