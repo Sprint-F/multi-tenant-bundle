2c5a83c4996ccd49bccde15d0d2549b891edce23
Степанцев Альберт <albert.stepantsev@sprintf.ru>
2025-11-19 22:13:19 +0300

    + QueryTenantResolver

--- src/SprintFMultiTenantBundle.php
+++ src/SprintFMultiTenantBundle.php
@@ -2,6 +2,8 @@
 
 namespace SprintF\Bundle\MultiTenant;
 
+use SprintF\Bundle\MultiTenant\Resolver\QueryTenantResolver;
+use SprintF\Bundle\MultiTenant\Resolver\TenantResolverInterface;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -47,5 +49,15 @@ class SprintFMultiTenantBundle extends AbstractBundle
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         $container->import('../config/services.yaml');
+
+        switch ($config['resolver']) {
+            case 'query':
+                $builder->register(QueryTenantResolver::class)
+                    ->setAutowired(true)
+                    ->setAutoconfigured(true)
+                    ->setArgument('$parameterName', $config['query']['parameter']);
+                $builder->setAlias(TenantResolverInterface::class, QueryTenantResolver::class);
+                break;
+        }
     }
 }
